ceb1d9c78fad4d6eb0894e864814b1e3ded9f993
陳奕儒 <jack12777@gmail.com>
2017-02-07 11:36:42 +0800

    hibernate pom.xml

---
+++ GSStravel/src/main/java/model/HibernateUtil.java
@@ -0,0 +1,24 @@
+package model;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+public class HibernateUtil {
+	private static final SessionFactory sessionFactory;
+
+	static {
+		try {
+			ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(new Configuration().configure().getProperties()).build();
+			sessionFactory = new Configuration().configure().buildSessionFactory(serviceRegistry);
+		} catch (Throwable ex) {
+			System.err.println("Initial SessionFactory creation failed." + ex);
+			throw new ExceptionInInitializerError(ex);
+		}
+	}
+
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+}
